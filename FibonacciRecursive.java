680a0804a4b03a6f56a22bb0afa71fb569243a93
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-08-26 13:34:29 -0700

    Finished implementing Fibonacci Recursive

--- FibonacciRecursive.java
+++ FibonacciRecursive.java
@@ -1,5 +1,11 @@
-public class FibonacciRecursive {
-	public static int fibonacci(int n) {
-
+public class FibonacciRecursive implements Fibonacci {
+	public int fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;	
+		}
+		return fibonacci(n-1) + fibonacci(n-2);	
 	}
 }
